9153be6dd4513bc36132807ac303674254aed35c
vishnudxb <vishnudxb@protonmail.com>
2020-12-31 14:37:36 -0500

    Cancel duplicate builds

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,12 +9,7 @@ BRANCH_NAME=$4
 
 # Check the status of the branch
 
-branch_status=$(curl \
-  -s
-  -X GET \
-  -H "Accept: application/vnd.github.v3+json" \
-  -H "Authorization: token $TOKEN" \
-  "https://api.github.com/repos/$REPO/actions/runs?branch=$BRANCH_NAME&status=in_progress" | jq '.workflow_runs[].status')
+branch_status=$(curl -s -X GET -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $TOKEN" "https://api.github.com/repos/$REPO/actions/runs?branch=$BRANCH_NAME&status=in_progress" | jq '.workflow_runs[].status')
 
 if [ "$branch_status" -eq "in_progress" ]
 then
@@ -22,4 +17,4 @@ then
   curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $TOKEN" https://api.github.com/repos/$REPO/actions/runs/$ID/cancel
 else
   echo "$BRANCH_NAME is not running, so continuing with this build"  
-fi 
+fi
