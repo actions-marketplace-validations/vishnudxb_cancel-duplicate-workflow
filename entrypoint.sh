e1ac6c6554a43788d6f9dc2bd08c8382758bb500
vishnudxb <vishnudxb@protonmail.com>
2020-12-31 16:45:15 -0500

    Cancel duplicate builds

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
@@ -9,9 +9,9 @@ BRANCH_NAME=$4
 
 # Check the status of the branch
 
-export STATUS=$(curl -s -X GET -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $TOKEN" "https://api.github.com/repos/$REPO/actions/runs?branch=$BRANCH_NAME&status=in_progress" | jq -r '.workflow_runs[].status')
+STATUS=$(curl -s -X GET -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $TOKEN" "https://api.github.com/repos/$REPO/actions/runs?branch=$BRANCH_NAME&status=in_progress" | jq -r '.workflow_runs[].status')
 
-if [[ "$STATUS" == "in_progress" ]];
+if [ "${STATUS}" == "in_progress" ];
 then
   echo "$BRANCH_NAME jobs are running, so cancelling this build..."  
   curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $TOKEN" https://api.github.com/repos/$REPO/actions/runs/$ID/cancel
