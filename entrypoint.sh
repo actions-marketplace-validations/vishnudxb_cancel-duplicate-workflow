dd3f13bf30c709efa109cbc3c1c714e5372ee021
vishnudxb <vishnudxb@protonmail.com>
2020-12-31 15:44:14 -0500

    Cancel duplicate builds

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ BRANCH_NAME=$4
 
 # Check the status of the branch
 
-branch_status=$(curl -s -X GET -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $TOKEN" "https://api.github.com/repos/$REPO/actions/runs?branch=$BRANCH_NAME&status=in_progress" | jq '.workflow_runs[].status')
+branch_status=$(curl -s -X GET -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $TOKEN" "https://api.github.com/repos/$REPO/actions/runs?branch=$BRANCH_NAME&status=in_progress" | jq -r '.workflow_runs[].status')
 
 if [[ "$branch_status" == "in_progress" ]];
 then
